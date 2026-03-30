98d85bc66a2c596721f080890cb69cefbc4d6ea7
josejrval <j.oliveirafilms@gmail.com>
2026-03-30 00:14:50 -0700

    Update HTML file served and fix webhook URL

--- server.js
+++ server.js
@@ -9,21 +9,18 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-// SERVIR O HTML
-app.use(express.static(path.join(__dirname, 'public')));
-
+// 👉 SERVE O HTML DIRETO
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'brand_film_luxury_v7.html'));
+  res.sendFile(path.join(__dirname, 'brand_film_luxury_v5.html'));
 });
 
+// 👉 RECEBE LEAD
 app.post('/lead', async (req, res) => {
   const leadData = req.body;
   console.log('NOVO LEAD:', leadData);
 
-  const MAKE_WEBHOOK_URL = 'https://hook.us2.make.com/6n477rbaq6fqqw30myrbtw0w57v3ktxv';
-
   try {
-    await fetch(MAKE_WEBHOOK_URL, {
+    await fetch('https://hook.us2.make.com/6n477rbaq6fqqw30myrbtw0w57v3ktxv', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(leadData)
